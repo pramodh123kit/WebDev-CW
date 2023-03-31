72a13649b78f8a65b83d35b434aad9157fd90579
Pramodh <pramodh123kit@gmail.com>
2023-03-31 17:32:19 +0530

    Made the content page/ Updated stlying in my pages

--- Gallery/Javascripts/Settings.js
+++ Gallery/Javascripts/Settings.js
@@ -1,47 +1,81 @@
 // Font size changer
-document.getElementById( "small" ).addEventListener( "click", function() {
-  document.getElementsByTagName("html")[0].style.fontSize = "15px";
+document.getElementById("small").addEventListener("click", function () {
+  document.getElementsByTagName("html")[0].style.fontSize = "18px";
 });
 
-document.getElementById( "medium" ).addEventListener( "click", function() {
-  document.getElementsByTagName("html")[0].style.fontSize = "20px";
+document.getElementById("medium").addEventListener("click", function () {
+  document.getElementsByTagName("html")[0].style.fontSize = "21px";
 });
 
-document.getElementById( "large" ).addEventListener( "click", function() {
-  document.getElementsByTagName("html")[0].style.fontSize = "25px";
+document.getElementById("large").addEventListener("click", function () {
+  document.getElementsByTagName("html")[0].style.fontSize = "24px";
 });
 
+// Random Color generator
+function RandomRGB() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let rgbColor = "rgb(" + red + "," + green + "," + blue + ")";
+  return rgbColor;
+}
+
+document.getElementById("random").addEventListener("click", function () {
+  let randomBackground = RandomRGB();
+
+  document.documentElement.style.setProperty("--background", randomBackground);
+  let h1Array = document.getElementsByTagName("h1");
+
+  for (let i = 0; i < h1Array.length; i += 1) {
+    if (i == 0) {
+      h1Array[0].style.color = "#000";
+    } else if (i == 11) {
+      h1Array[11].style.color = "#fff";
+    } else if (i > 0 && i <= 10 ) {
+      h1Array[i].style.color = randomBackground;
+    }
+  }
+});
 
 // Color changer
-document.getElementById( "dark" ).addEventListener( "click", function() {
-  document.documentElement.style.setProperty('--background', '#121212');
-
-  let h1Array = document.getElementsByTagName("h1");
-
-  for (let i = 0 ; i < h1Array.length ; i += 1) {
-    h1Array[i].style.color = '#FFFF00';
-  }
-});
-
-
-
-document.getElementById( "light" ).addEventListener( "click", function() {
-  document.documentElement.style.setProperty('--background', '#fff');
-  
-  let h1Array = document.getElementsByTagName("h1");
-
-  for (let i = 0 ; i < h1Array.length ; i += 1) {
-    h1Array[i].style.color = '#a7dd13';
-  }
-});
-
-
-document.getElementById( "lightblue" ).addEventListener( "click", function() {
-  document.documentElement.style.setProperty('--background', '#00A9A5');
+document.getElementById("dark").addEventListener("click", function () {
+  document.documentElement.style.setProperty("--background", "#121212");
  
   let h1Array = document.getElementsByTagName("h1");
 
-  for (let i = 0 ; i < h1Array.length ; i += 1) {
-    h1Array[i].style.color = '#000';
+  for (let i = 0; i < h1Array.length; i += 1) {
+    if (i == 0) {
+      h1Array[0].style.color = "#000";
+    } else if (i == 11) {
+      h1Array[11].style.color = "#ff0";
+    } else if (i > 0 && i <= 10 ) {
+      h1Array[i].style.color = "rgb(253,183,10)";
+    }
+  }
+});
+
+document.getElementById("light").addEventListener("click", function () {
+  document.documentElement.style.setProperty("--background", "#fff");
+
+  let h1Array = document.getElementsByTagName("h1");
+
+  for (let i = 0; i < h1Array.length; i += 1) {
+      h1Array[i].style.color = "#000";
+  }
+});
+
+document.getElementById("yellow").addEventListener("click", function () {
+  document.documentElement.style.setProperty("--background", "rgb(253,183,10)");
+
+  let h1Array = document.getElementsByTagName("h1");
+
+  for (let i = 0; i < h1Array.length; i += 1) {
+    if (i == 0) {
+      h1Array[0].style.color = "#000";
+    } else if (i == 11) {
+      h1Array[11].style.color = "#fff";
+    } else if (i > 0 && i <= 10 ) {
+      h1Array[i].style.color = "rgb(253,183,10)";
+    }
   }
 });
